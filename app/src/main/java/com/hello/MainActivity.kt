4190e3a635c549190872ee962190ec9e227c9491
jonathantwiggins <67426347+jonathantwiggins@users.noreply.github.com>
2024-07-07 20:20:52 -0500

    Initial Commit with working Speak

---
+++ app/src/main/java/com/hello/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.hello
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hello.ui.theme.HelloTheme
+import android.speech.tts.TextToSpeech
+import java.util.Locale
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : ComponentActivity(), TextToSpeech.OnInitListener {
+
+    private lateinit var textToSpeech: TextToSpeech
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        textToSpeech = TextToSpeech(this, this)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val text = editText.text.toString()
+            speakOut(text)
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                // Handle the error
+            } else {
+                // Text-to-Speech is ready to use
+            }
+        } else {
+            // Initialization failed
+        }
+    }
+
+    private fun speakOut(text: String) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+        super.onDestroy()
+    }
+}
